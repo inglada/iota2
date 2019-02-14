bd401e0a790250ffc2bbe717e1d4c9a154678e6d
Arthur VINCENT <arthur.vincent@outlook.fr>
2019-02-14 13:45:29 +0000

    ENH : User_features.get_features() available

--- scripts/Sensors/User_features.py
+++ scripts/Sensors/User_features.py
@@ -33,10 +33,51 @@ class User_features(Sensor):
     def __init__(self, config_path, tile_name):
         """
         """
-        pass
+        from Common import ServiceConfigFile as SCF
 
+        self.tile_name = tile_name
+        self.cfg_IOTA2 = SCF.serviceConfigFile(config_path)
+        self.user_feat_data = self.cfg_IOTA2.getParam("chain", "userFeatPath")
+        tile_dir_name = [dir_name for dir_name in os.listdir(self.user_feat_data) if tile_name in dir_name][0]
 
+        # run attributes
+        self.tile_directory = os.path.join(self.user_feat_data, tile_dir_name)
+        self.features_dir = os.path.join(self.cfg_IOTA2.getParam("chain", "outputPath"),
+                                         "features", tile_name)
+        # sensors attributes
+        self.data_type = self.cfg_IOTA2.getParam("userFeat", "patterns").replace(" ","").split(",")
+
+        # output's names
+        self.features_names = "{}_{}_Features.tif".format(self.__class__.name,
+                                                          tile_name)
     def get_features(self, ram=128, logger=logger):
         """
         """
-        pass
+        from Common.OtbAppBank import CreateConcatenateImagesApplication
+        from Common.FileUtils import FileSearch_AND
+        from Common.FileUtils import ensure_dir
+
+        features_dir = os.path.join(self.features_dir, "tmp")
+        ensure_dir(features_dir, raise_exe=False)
+        features_out = os.path.join(features_dir, self.features_names)
+
+        user_features = []
+        for pattern in self.data_type:
+            user_feature = FileSearch_AND(self.tile_directory, True, pattern)
+            if user_feature:
+                user_features.append(user_feature[0])
+            else :
+                msg = "WARNING : '{}' not found in {}".format(pattern, self.tile_directory)
+                logger.error(msg)
+                raise Exception(msg)
+
+        user_feat_stack = CreateConcatenateImagesApplication({"il": user_features,
+                                                              "ram": str(ram),
+                                                              "out": features_out})
+        features_labels = [pattern for pattern in self.data_type]
+        return (user_feat_stack, []), features_labels
+
+
+
+
+        
