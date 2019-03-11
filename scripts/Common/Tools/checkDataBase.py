4439ae2f66b65153c834dcf3e8897f26c2b3a6e8
Arthur VINCENT <arthur.vincent@outlook.fr>
2019-03-11 13:20:24 +0000

    WIP : checkDataBase workflow

--- scripts/Common/Tools/checkDataBase.py
+++ scripts/Common/Tools/checkDataBase.py
@@ -32,6 +32,33 @@ def str2bool(v):
     else:
         raise argparse.ArgumentTypeError('Boolean value expected.')
 
+def vector_name_check(input_vector):
+    """
+    check if first character is a letter
+    """
+    import string
+    avail_characters = string.ascii_letters
+    first_character = os.path.basename(input_vector)[0]
+    return True if first_character in avail_characters else False
+
+
+def remove_invalid_features(shapefile):
+    
+    # remove invalid features
+    driver = ogr.GetDriverByName("ESRI Shapefile")
+    dataSource = driver.Open(shapefile, 1)
+    if not dataSource:
+        raise
+    layer = dataSource.GetLayer()
+    count=layer.GetFeatureCount()
+    for feature in range(count):
+        feat = layer[feature]
+        geom = feat.GetGeometryRef()
+        if not geom:
+            layer.DeleteFeature(feature)
+    layer.ResetReading()
+
+
 def do_check(input_vector, output_vector, data_field, epsg, pix_area,
              pix_area_threshold, do_corrections):
     """
@@ -43,15 +70,49 @@ def do_check(input_vector, output_vector, data_field, epsg, pix_area,
     input_vector_fields = getFields(input_vector)
 
     errors = []
+    # check vector's name
+    name_check = vector_name_check(input_vector)
+    if name_check is False:
+        error_msg = "file's name not correct"
+        errors.append(error_msg)
+
+    # check field
     if not data_field in input_vector_fields:
         error_msg = "field '{}' not found".format(data_field)
         errors.append(error_msg)
 
+    # check field's type
     label_field_type = getFieldType(input_vector, data_field)
     if not label_field_type is int:
         error_msg = "field '{}' is not containing intergers".format(data_field)
         errors.append(error_msg)
-    print errors
+
+    # geometries checks 
+    #~ outShapefileGeom = vf.checkEmptyGeom(shapefile)
+    #~ outShapefileGeom = vf.checkEmptyGeom(shapefile, outputShapeFile, do_corrections)
+    
+    # suppression des doubles géométries
+    #~ DeleteDuplicateGeometriesSqlite.deleteDuplicateGeometriesSqlite(outShapefileGeom)
+    #~ DeleteDuplicateGeometriesSqlite.deleteDuplicateGeometriesSqlite(inputShapeFile, outShapefileGeom, do_corrections)
+    
+    # Suppression des multipolygons
+    #~ MultiPolyToPoly.multipoly2poly(outShapefileGeom, shapefileNoDupspoly)
+    #~ MultiPolyToPoly.multipoly2poly(inputshape, outputShape, do_corrections)
+
+    # recalcul des superficies, on est obligé d'ajouter une nouvelle colonne ?
+    #~ AddFieldArea.addFieldArea(shapefileNoDupspoly, pixelArea)
+
+    # Filter by Area
+    #~ SelectBySize.selectBySize(shapefileNoDupspoly, 'Area', pix_thresh, outshape)
+    #~ SelectBySize.selectBySize(shapefileNoDupspoly, pix_thresh, outshape, do_corrections)
+
+    # Check valid geometry
+    #~ vf.checkValidGeom(outshape)
+    #~ vf.checkValidGeom(inputShape, outshape)
+
+    # remove features with empty geometries
+    #~ remove_invalid_features(shapefile)
+    #~ remove_invalid_features(shapefile, outputshape, do_corrections)
     return errors
 
 if __name__ == "__main__":
