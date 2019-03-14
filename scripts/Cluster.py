9840facb1c03d125bc6bc1e5b28e8c3c10cd367b
Arthur VINCENT <arthur.vincent@outlook.fr>
2019-03-14 14:34:08 +0000

    ENH : add new error pattern

--- scripts/Cluster.py
+++ scripts/Cluster.py
@@ -306,7 +306,7 @@ def check_errors(log_path):
             if error_find:
                 errors.append([error for error in error_find])
     """
-    err_pattern = ["Traceback", "PBS: job killed:", ": fail", "Segmentation fault"]
+    err_pattern = ["Traceback", "PBS: job killed:", ": fail", "Segmentation fault", "OpenCV Error"]
     with open(log_path, "r") as log_err:
         for line in log_err:
             for err_patt in err_pattern:
