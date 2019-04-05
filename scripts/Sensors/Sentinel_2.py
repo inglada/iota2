8472e7027c6fa1690d4138088d07decc780797df
Arthur VINCENT <arthur.vincent@outlook.fr>
2019-04-05 11:31:55 +0000

    ENH : Sentinel-2 overwrite masks if wrong projection

--- scripts/Sensors/Sentinel_2.py
+++ scripts/Sensors/Sentinel_2.py
@@ -275,7 +275,11 @@ class Sentinel_2(Sensor):
         app_dep = [binary_mask_rule]
 
         if self.write_dates_stack:
-            if not os.path.exists(out_mask):
+            same_proj = False
+            if os.path.exists(out_mask):
+                same_proj = int(getRasterProjectionEPSG(out_mask)) == int(self.target_proj)
+
+            if not os.path.exists(out_mask) or same_proj is False:
                 superimp.ExecuteAndWriteOutput()
                 if working_dir:
                     shutil.copy(out_mask_processing, out_mask)
