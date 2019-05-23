ff60b50491fa59b40491735214536a86526ced03
Arthur VINCENT <arthur.vincent@outlook.fr>
2019-05-23 09:05:15 +0000

    ENH : add setup.py autogenerated by PyCharm

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='iota2',
+    version='',
+    packages=['scripts', 'scripts.MPI', 'scripts.Steps', 'scripts.Tests', 'scripts.Tests.UnitTests',
+              'scripts.Tests.IntegrationTests', 'scripts.Common', 'scripts.Common.Tools', 'scripts.Sensors',
+              'scripts.Sensors.SAR', 'scripts.Learning', 'scripts.Sampling', 'scripts.Validation',
+              'scripts.VectorTools', 'scripts.Classification', 'scripts.simplification'],
+    url='',
+    license='',
+    author='Arthur VINCENT',
+    author_email='',
+    description=''
+)
