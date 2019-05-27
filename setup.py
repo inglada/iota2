1f2986c4c379e7c3ce65bd47bdcf5a3b8bf52d36
Arthur VINCENT <arthur.vincent@outlook.fr>
2019-05-27 14:25:22 +0000

    INSTALL : remove py_modules

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
     name='iota2',
     version='',
-    py_modules = ["iota2"],
+    #~ py_modules = ["iota2"],
     #~ packages=['iota2', 'iota2.MPI', 'iota2.Steps', 'iota2.Tests', 'iota2.Tests.UnitTests',
               #~ 'iota2.Tests.IntegrationTests', 'iota2.Common', 'iota2.Common.Tools', 'iota2.Sensors',
               #~ 'iota2.Sensors.SAR', 'iota2.Learning', 'iota2.Sampling', 'iota2.Validation',
