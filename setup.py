5f818c120cb5abc392a8e0403742a5eec90601f6
Arthur VINCENT <arthur.vincent@outlook.fr>
2019-06-07 12:31:20 +0000

    INSTALL : setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,12 @@
-#~ from distutils.core import setup
 import setuptools
 setuptools.setup(
     name='iota2',
     version='',
-    #~ py_modules = ["iota2"],
-    #~ packages=['iota2', 'iota2.MPI', 'iota2.Steps', 'iota2.Tests', 'iota2.Tests.UnitTests',
-              #~ 'iota2.Tests.IntegrationTests', 'iota2.Common', 'iota2.Common.Tools', 'iota2.Sensors',
-              #~ 'iota2.Sensors.SAR', 'iota2.Learning', 'iota2.Sampling', 'iota2.Validation',
-              #~ 'iota2.VectorTools', 'iota2.Classification', 'iota2.simplification'],
     packages=setuptools.find_packages(),
-    url='',
-    license='',
-    author='Arthur VINCENT',
+    url='https://framagit.org/iota2-project/iota2',
+    license='GNU Affero General Public License v3.0',
+    author='iota2 development team',
     author_email='',
-    description=''
+    description=("Infrastructure pour l'Occupation des sols par Traitement "
+                 "Automatique Incorporant les Orfeo Toolbox Applications")
 )
