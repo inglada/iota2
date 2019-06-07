8e38d444ec992530a0355d2579d4179f1cc3fc0c
Arthur VINCENT <arthur.vincent@outlook.fr>
2019-06-07 13:28:16 +0000

    INSTALL : include package data

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setuptools.setup(
     url='https://framagit.org/iota2-project/iota2',
     license='GNU Affero General Public License v3.0',
     author='iota2 development team',
+    include_package_data=True,
     author_email='',
     description=("Infrastructure pour l'Occupation des sols par Traitement "
                  "Automatique Incorporant les Orfeo Toolbox Applications")
