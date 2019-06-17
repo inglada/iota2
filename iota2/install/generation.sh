3df29860402d3a861ecdfbdd228c37a50be7bfd8
Arthur VINCENT <arthur.vincent@outlook.fr>
2019-06-17 09:22:03 +0000

    INSTALL

--- iota2/install/generation.sh
+++ iota2/install/generation.sh
@@ -33,10 +33,9 @@ SH_DIR="$(dirname "$CMD")"
 echo $SH_DIR
 prefix_dir=$SH_DIR
 ok=0
+OTB_VERSION='6.6.2'
 
-OTB_VERSION='6.6.1'
-OTB_DEV_COMMIT='0df44b312d64d6c3890b65d3790d4a17d0fd5f23'
-
+OTB_DEV_COMMIT='7b09c7e4e171ba77a0021eef6bc959f59c4758d5'
 
 if [ ! -z $CXX ]; then
   echo "Compiler used : $CXX"
@@ -84,17 +83,54 @@ if [[ "$ok" == "1" ]]; then
         git clone -b develop https://github.com/orfeotoolbox/OTB.git
         cd OTB
         git checkout $OTB_DEV_COMMIT
-        # due to otb's issue https://gitlab.orfeo-toolbox.org/orfeotoolbox/otb/issues/1899
-        git revert -m 1 3df7fcd24cd75bbfd3cf68c0cab06ab25e68bbfd --strategy=recursive -Xours
         #~ git checkout release-6.6
       fi
 
       echo "Getting Superbuild archives ..."
       cd $prefix_dir/OTB
       mkdir -p SuperBuild-archives
+      cd SuperBuild-archives
+      if [ -f "./SuperBuild-archives-${OTB_VERSION}.tar.bz2" ]; then
+        echo "SuperBuild archives already downloaded. skipping."
+      else
+        wget https://www.orfeo-toolbox.org/packages/SuperBuild-archives-${OTB_VERSION}.tar.bz2
+        wget https://www.orfeo-toolbox.org/packages/SuperBuild-archives-${OTB_VERSION}.md5
+        md5sum SuperBuild-archives-${OTB_VERSION}.tar.bz2 > verif_MD5
+        nbDiff=`diff verif_MD5 SuperBuild-archives-${OTB_VERSION}.md5 | wc -l`
+        if [[ "$nbDiff" == 0 ]]; then
+          tar -xvjf SuperBuild-archives-${OTB_VERSION}.tar.bz2
+        else
+          echo "MD5sum is different for SuperBuild-archives-${OTB_VERSION}.tar.bz2 file !"
+          echo "Maybe a problem during download ?"
+          exit
+        fi
+      fi
     fi
-
     if [[ "$#" == 1 ]] || [[ "$2" == "iota2" ]]; then
+      # Getting GapFilling source files
+      echo "Adding OTBGapFilling module ..."
+      mkdir -p $prefix_dir/CESBIO
+      cd $prefix_dir/CESBIO
+      if [ -d "./temporalgapfilling" ]; then
+        echo "temporalgapfilling repository already cloned. skipping."
+      else
+        git clone https://gitlab.orfeo-toolbox.org/jinglada/temporalgapfilling.git
+      fi
+      cd $prefix_dir/OTB/OTB/Modules/Remote/
+      ln -sf ../../../../CESBIO/temporalgapfilling OTBTemporalGapFilling                                                                                   
+      # Getting MultitempFiltering source files
+      echo "Adding MultitempFiltering module ..."
+      mkdir -p $prefix_dir/CESBIO
+      cd $prefix_dir/CESBIO
+      if [ -d "./otb-for-biomass" ]; then
+        echo "MultitempFiltering repository already cloned. skipping."
+      else
+        echo "get otb-for-biomass"
+        git clone -b memChain https://framagit.org/ArthurV/otb-for-biomass.git
+      fi
+      cd $prefix_dir/OTB/OTB/Modules/Remote/
+      ln -sf ../../../../CESBIO/otb-for-biomass/MultitempFiltering MultitempFiltering
+ 
       # Add iota2 module
       echo "Adding iota2 module ..."
       cd $prefix_dir/CESBIO
@@ -103,7 +139,7 @@ if [[ "$ok" == "1" ]]; then
       else
       # symbolic link to iota2 main repository already cloned
       # this to avoid misunderstanding after, when running iota2.
-      # git clone https://framagit.org/iota2-project/iota2
+#        git clone https://framagit.org/iota2-project/iota2
         ln -s $prefix_dir/../../. iota2
       fi
       cd $prefix_dir/OTB/OTB/Modules/Remote/
@@ -123,7 +159,6 @@ if [[ "$ok" == "1" ]]; then
       ln -sf ../../../../TETIS/otbPointMatchCoregistrationModel OTBPointMatchCoregistrationModel
     fi
   fi
-
   #----------------------------------------
   # Building
   if [[ "$1" == "--compil" ]] || [[ "$1" == "--all" ]]; then  
@@ -133,15 +168,16 @@ if [[ "$ok" == "1" ]]; then
       cd $prefix_dir/OTB
       mkdir -p build
       cd build
-      cmake -DOTB_USE_SHARK=ON -DCMAKE_CXX_COMPILER=${CXX} -DCMAKE_CXX_FLAGS:STRING=-std=c++14 -DUSE_SYSTEM_BOOST=ON -DBUILD_TESTING=OFF -DCMAKE_BUILD_TYPE=Release -DOTB_WRAP_PYTHON:BOOL=ON -DGDAL_SB_EXTRA_OPTIONS:STRING="--with-python=/usr/bin/python3" -DCMAKE_INSTALL_PREFIX=$prefix_dir/OTB/install/ -DDOWNLOAD_LOCATION=$prefix_dir/OTB/SuperBuild-archives/ -DOTB_USE_QT=OFF -DOTB_USE_QWT=OFF -DOTB_USE_GLEW=OFF -DOTB_USE_GLUT=OFF -DOTB_USE_OPENGL=OFF $prefix_dir/OTB/OTB/SuperBuild/
-      make -j 4
+      cmake -DCMAKE_CXX_COMPILER=${CXX} -DCMAKE_CXX_FLAGS:STRING=-std=c++14 -DUSE_SYSTEM_BOOST=ON -DBUILD_TESTING=OFF -DCMAKE_BUILD_TYPE=Release -DOTB_WRAP_PYTHON:BOOL=ON -DGDAL_SB_EXTRA_OPTIONS:STRING="--with-python" -DCMAKE_INSTALL_PREFIX=$prefix_dir/OTB/install/ -DDOWNLOAD_LOCATION=$prefix_dir/OTB/SuperBuild-archives/ -DOTB_USE_QT=OFF -DOTB_USE_QWT=OFF -DOTB_USE_GLEW=OFF -DOTB_USE_GLUT=OFF -DOTB_USE_OPENGL=OFF $prefix_dir/OTB/OTB/SuperBuild/
+      make
+#      make VERBOSE=1 -j2
     fi
     if [[ "$#" == 1 ]] || [[ "$2" == "iota2" ]]; then 
       # Building iota2
       echo "Building iota2 ..."
       cd $prefix_dir/OTB/build/OTB/build
-      cmake -DOTB_USE_SHARK=ON -DCMAKE_CXX_COMPILER=${CXX} -DCMAKE_CXX_FLAGS:STRING=-std=c++14 -DModule_IOTA2:BOOL=ON -DModule_OTBTemporalGapFilling:BOOL=ON -DModule_MultitempFiltering:BOOL=ON -DModule_OTBAppPointMatchCoregistration:BOOL=ON $prefix_dir/OTB/OTB 
-      make -j 4
+      cmake -DCMAKE_CXX_COMPILER=${CXX} -DCMAKE_CXX_FLAGS:STRING=-std=c++14 -DModule_IOTA2:BOOL=ON -DModule_IOTA2:BOOL=ON -DModule_OTBTemporalGapFilling:BOOL=ON -DModule_MultitempFiltering:BOOL=ON -DModule_OTBAppPointMatchCoregistration:BOOL=ON $prefix_dir/OTB/OTB 
+      make
       make install
     fi
   fi
