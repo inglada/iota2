0684d8b1aff176c2f3a3a1f4bda2eb129eeb786c
Arthur VINCENT <arthur.vincent@c-s.fr>
2019-09-03 09:33:00 +0000

    WIP : add the new step 'slicSegmentation'

---
+++ iota2/Segmentation/segmentation.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+
+# =========================================================================
+#   Program:   iota2
+#
+#   Copyright (c) CESBIO. All rights reserved.
+#
+#   See LICENSE for details.
+#
+#   This software is distributed WITHOUT ANY WARRANTY; without even
+#   the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+#   PURPOSE.  See the above copyright notices for more information.
+#
+# =========================================================================
+
+def slicSegmentation(tile_name, config_path):
+    """
+    """
+    pause = input("slicSegmentation")
