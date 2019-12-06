001d9f9b1b257baa8a141918078fa76c8926f53e
Thierion <vincent.thierion@cesbio.cnes.fr>
2019-12-06 14:53:48 +0000

    ENH : modify regularization step to take into account changes in regularization process

--- iota2/Steps/Regularization.py
+++ iota2/Steps/Regularization.py
@@ -20,23 +20,25 @@ from Cluster import get_RAM
 from Common import ServiceConfigFile as SCF
 
 class Regularization(IOTA2Step.Step):
-    def __init__(self, cfg, cfg_resources_file, workingDirectory=None):
+    def __init__(self, cfg, cfg_resources_file, umc, logname=None, workingDirectory=None):
         # heritage init
         resources_block_name = "regularisation"
         super(Regularization, self).__init__(cfg, cfg_resources_file, resources_block_name)
 
+        
         # step variables
+        if logname:
+            self.logFile = logname
+
         self.RAM = 1024.0 * get_RAM(self.resources["ram"])
         self.CPU = self.resources["cpu"]
         self.workingDirectory = workingDirectory
         self.outputPath = SCF.serviceConfigFile(self.cfg).getParam('chain', 'outputPath')
         self.rastclass = SCF.serviceConfigFile(self.cfg).getParam('Simplification', 'classification')
         self.seed = SCF.serviceConfigFile(self.cfg).getParam('Simplification', 'seed')
-        self.umc1 = SCF.serviceConfigFile(self.cfg).getParam('Simplification', 'umc1')
-        self.inland = SCF.serviceConfigFile(self.cfg).getParam('Simplification', 'inland')
-        self.rssize = SCF.serviceConfigFile(self.cfg).getParam('Simplification', 'rssize')
-        self.umc2 = SCF.serviceConfigFile(self.cfg).getParam('Simplification', 'umc2')
-
+        self.umc = umc
+        self.outtmpdir = os.path.join(self.outputPath, 'final', 'simplification', 'tmp')
+        
     def step_description(self):
         """
         function use to print a short description of the step's purpose
@@ -50,17 +52,27 @@ class Regularization(IOTA2Step.Step):
         ------
             the return could be and iterable or a callable
         """
-        rastclass = self.rastclass
-        if rastclass is None:
-            if self.seed is not None:
-                rastclass = os.path.join(self.outputPath, 'final', 'Classif_Seed_{}.tif'.format(seed))
-            else:
-                if os.path.exists(os.path.join(self.outputPath, 'final', 'Classifications_fusion.tif')):
-                    rastclass = os.path.join(self.outputPath, 'final', 'Classifications_fusion.tif')
-                else:
-                    rastclass = os.path.join(self.outputPath, 'final', 'Classif_Seed_0.tif')
 
-        return [rastclass]
+        from simplification import manageNomenclature as mn
+        
+        rastclass = self.rastclass
+        if not os.path.exists(os.path.join(self.outtmpdir, 'regul1.tif')):
+            if rastclass is None:
+                if self.seed is not None:
+                    rastclass = os.path.join(self.outputPath, 'final', 'Classif_Seed_{}.tif'.format(seed))
+                else:
+                    if os.path.exists(os.path.join(self.outputPath, 'final', 'Classifications_fusion.tif')):
+                        rastclass = os.path.join(self.outputPath, 'final', 'Classifications_fusion.tif')
+                    else:
+                        rastclass = os.path.join(self.outputPath, 'final', 'Classif_Seed_0.tif')
+        else:
+            rastclass = os.path.join(self.outtmpdir, 'regul1.tif')
+                    
+        rules = mn.getMaskRegularisation("/home/qt/thierionv/dev/iota2/iota2/simplification/nomenclature.cfg")
+
+        scenarios = [[rastclass, rule] for rule in rules]
+
+        return scenarios
 
     def step_execute(self):
         """
@@ -70,26 +82,29 @@ class Regularization(IOTA2Step.Step):
             the function to execute as a lambda function. The returned object
             must be a lambda function.
         """
-        from simplification import Regularization as regul
+        from simplification import manageNomenclature as mn
 
-        tmpdir = self.workingDirectory
-        if tmpdir is None:
-            tmpdir = os.path.join(self.outputPath, 'final', 'simplification',
-                                  'tmp')
-        outfilereg = os.path.join(self.outputPath, 'final', 'simplification',
-                                  'classif_regul.tif')
-        step_function = lambda x: regul.OSORegularization(x,
-                                                          self.umc1,
-                                                          self.CPU,
-                                                          tmpdir,
-                                                          outfilereg,
-                                                          str(self.RAM),
-                                                          self.inland,
-                                                          self.rssize,
-                                                          self.umc2)
+        if self.workingDirectory:
+            self.tmpdir = self.workingDirectory        
+
+        #outfilereg = os.path.join(self.outputPath, 'final', 'simplification','classif_regul.tif')
+        
+        step_function = lambda x: mn.adaptRegularization(self.tmpdir,
+                                                         x[0],
+                                                         os.path.join(self.outtmpdir, x[1][2]),
+                                                         str(self.RAM),
+                                                         x[1],
+                                                         self.umc)
+
         return step_function
 
     def step_outputs(self):
         """
         """
+        pass
+
+    def step_clean(self):
+        """
+        """
+        
         pass
