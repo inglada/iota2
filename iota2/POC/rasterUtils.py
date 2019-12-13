1108c4506add136a2a7fcbfad27e8a4adef61cea
Arthur VINCENT <arthur.vincent@c-s.fr>
2019-12-13 15:07:38 +0000

    ENH : new split strategy, 'split_number'

--- iota2/POC/rasterUtils.py
+++ iota2/POC/rasterUtils.py
@@ -38,9 +38,12 @@ def apply_function(otb_pipeline: otbApplication,
                    output_path: Optional[str] = None,
                    mask: Optional[str] = None,
                    mask_value: Optional[int] = 0,
-                   chunk_size_mode: Optional[str] = "auto",
+                   chunk_size_mode: Optional[str] = "user_fixed",
                    chunck_size_x: Optional[int] = 10,
                    chunck_size_y: Optional[int] = 10,
+                   targeted_chunk: Optional[int] = None,
+                   number_of_chunks: Optional[int] = None,
+                   output_number_of_bands: Optional[int] = None,
                    ram: Optional[int] = 128,
                    logger=logger) -> Tuple[np.ndarray, List[str], Affine, int]:
     """
@@ -60,10 +63,16 @@ def apply_function(otb_pipeline: otbApplication,
         input mask path (optional)
     mask_value: int
         input mask value to consider (optional)
+    chunk_size_mode : str
+        "user_fixed" / "auto" / "split_number"
     chunck_size_x: int
         chunck x size (optional)
     chunck_size_y: int
         chunck y size (optional)
+    targeted_chunk : int
+        process only the targeted chunk
+    output_number_of_bands : int
+        use only if targeted_chunk and mask are set
     ram: int
         available ram
 
@@ -80,13 +89,18 @@ def apply_function(otb_pipeline: otbApplication,
                                           chunk_size_mode=chunk_size_mode,
                                           chunk_size=(chunck_size_x,
                                                       chunck_size_y),
+                                          number_of_chunks=number_of_chunks,
                                           ram_per_chunk=ram,
                                           working_dir=working_dir)
+    if targeted_chunk is not None:
+        roi_rasters = [roi_rasters[targeted_chunk]]
+
     mask_array = None
     if mask:
         mask_array = rasterToArray(mask)
 
     new_arrays = []
+    chunks_mask = []
     for index, roi_raster in enumerate(roi_rasters):
         start_x = int(roi_raster.GetParameterString("startx"))
         size_x = int(roi_raster.GetParameterString("sizex"))
@@ -100,18 +114,30 @@ def apply_function(otb_pipeline: otbApplication,
         logger.info(region_info)
         print(region_info)
         print("memory usage : {}".format(memory_usage_psutil()))
-        roi_array, proj_geotransform = process_function(roi_raster,
-                                                        function=function,
-                                                        mask_arr=mask_array,
-                                                        mask_value=mask_value,
-                                                        mask_box=(start_x,
-                                                                  size_x,
-                                                                  start_y,
-                                                                  size_y))
+        (roi_array, proj_geotransform), mask = process_function(roi_raster,
+                                                                function=function,
+                                                                mask_arr=mask_array,
+                                                                mask_value=mask_value,
+                                                                mask_box=(start_x,
+                                                                          size_x,
+                                                                          start_y,
+                                                                          size_y))
         new_arrays.append((roi_array, proj_geotransform))
+        chunks_mask.append(mask)
 
-    all_data_sets = get_rasterio_datasets(new_arrays, mask_value)
-    mosaic, out_trans = merge(all_data_sets)
+    all_data_sets = get_rasterio_datasets(new_arrays,
+                                          mask_value,
+                                          force_output_shape=(size_y,
+                                                              size_x,
+                                                              output_number_of_bands))
+    if len(all_data_sets) > 1:
+        mosaic, out_trans = merge(all_data_sets)
+    else:
+        if isinstance(new_arrays[0][0], int):
+            mosaic = np.repeat(mask[np.newaxis, :, :], output_number_of_bands, axis=0)
+        else:
+            mosaic = np.moveaxis(new_arrays[0][0], -1, 0)
+        out_trans = all_data_sets[0].transform
     if output_path:
         with rasterio.open(output_path,
                            "w",
@@ -123,20 +149,25 @@ def apply_function(otb_pipeline: otbApplication,
                            transform=out_trans,
                            dtype=mosaic.dtype) as dest:
             dest.write(mosaic)
-
     # TODO : new_labels definition
-    return mosaic, new_labels, out_trans, epsg_code
+    return mosaic, new_labels, out_trans, epsg_code, chunks_mask
 
 
 def get_rasterio_datasets(array_proj: List[Tuple[Union[np.ndarray, int], Dict]],
-                          mask_value: Optional[int] = 0) -> List[rasterio.io.DatasetReader]:
+                          mask_value: Optional[int] = 0,
+                          force_output_shape: Optional[Tuple[int, int, int]] = (1, 1, 1),
+                          force_output_type: Optional[str] = "int32") -> List[rasterio.io.DatasetReader]:
     """transform numpy arrays (containing projection data) to rasterio datasets
         it works only with 3D arrays
     """
     all_data_sets = []
     expected_arr_shapes = set([arr[0].shape for arr in array_proj if isinstance(arr[0], np.ndarray)])
+    if not expected_arr_shapes:
+        expected_arr_shapes = [force_output_shape]
     expected_arr_shape = list(expected_arr_shapes)[0]
     expected_arr_types = set([arr[0].dtype for arr in array_proj if isinstance(arr[0], np.ndarray)])
+    if not expected_arr_types:
+        expected_arr_types = [force_output_type]
     expected_arr_type = list(expected_arr_types)[0]
 
     for index, new_array in enumerate(array_proj):
@@ -227,23 +258,25 @@ def process_function(otb_pipeline: otbApplication,
                      0,
                      yres]
 
+    
     if roi_to_ignore is False:
         output_arr = function(array)
         if roi_contains_mask_part:
-            mask_roi = np.repeat(mask_roi[:, :, np.newaxis], output_arr.shape[-1], axis=2)
-            output_arr = output_arr * mask_roi
+            mask_roi_bands = np.repeat(mask_roi[:, :, np.newaxis], output_arr.shape[-1], axis=2)
+            output_arr = output_arr * mask_roi_bands
         output = (output_arr,
                   {"projection": projection, "geo_transform": geo_transform})
     else:
         output = (mask_value,
                   {"projection": projection, "geo_transform": geo_transform})
     otb_pipeline = None
-    return output
+    return output, mask_roi
 
 
 def get_chunks_boundaries(chunk_size: Tuple[int, int],
                           shape: Tuple[int, int],
                           chunk_size_mode: str,
+                          number_of_chunks: int,
                           ram_estimation: int,
                           ram_per_chunk: int) -> List[Dict[str, int]]:
     """from numpy array shape, return chunks boundaries (Extract ROI coordinates)
@@ -256,6 +289,8 @@ def get_chunks_boundaries(chunk_size: Tuple[int, int],
         tuple(size_x, size_y)
     chunk_size_mode : str
         flag
+    number_of_chunks : int
+        use if chunk_size_mode is "split_number"
     ram_estimation : float
         ram estimation to compute the whole OTB process (in octets)
     ram_per_chunk : float
@@ -279,6 +314,9 @@ def get_chunks_boundaries(chunk_size: Tuple[int, int],
         nb_chunks = math.ceil(ram_estimation / ram_per_chunk)
         chunk_size_x = size_x
         chunk_size_y = int(math.ceil(float(size_y) / float(nb_chunks)))
+    elif chunk_size_mode == "split_number":
+        chunk_size_x = size_x
+        chunk_size_y = int(math.ceil(float(size_y) / float(number_of_chunks)))
 
     boundaries = []
     for y in np.arange(0, size_y, chunk_size_y):
@@ -298,6 +336,7 @@ OTB_CHUNK = Tuple[type(otbApplication), int]
 def split_raster(otb_pipeline: otbApplication,
                  chunk_size_mode: str,
                  chunk_size: Tuple[int, int],
+                 number_of_chunks: int,
                  ram_per_chunk: int,
                  working_dir: str) -> List[OTB_CHUNK]:
     """extract regions of interest over the otbApplication
@@ -317,7 +356,6 @@ def split_raster(otb_pipeline: otbApplication,
     from iota2.Common.OtbAppBank import CreateExtractROIApplication
 
     otb_pipeline.Execute()
-
     proj = otb_pipeline.GetImageProjection("out")
     projection = osr.SpatialReference()
     projection.ImportFromWkt(proj)
@@ -328,9 +366,16 @@ def split_raster(otb_pipeline: otbApplication,
     ram_estimation = otb_pipeline.PropagateRequestedRegion(key="out",
                                                            region=otb_pipeline.GetImageRequestedRegion("out"))
 
+    print("x_size : {} et y_size : {}".format(x_size, y_size))
+
+    roi = CreateExtractROIApplication({"in": otb_pipeline,
+                                       "cl": ["Channel1"],
+                                       "ram": "60000",
+                                       "out": "/work/OT/theia/oso/arthur/TMP/test_ROI.tif"})
     boundaries = get_chunks_boundaries(chunk_size,
                                        shape=(x_size, y_size),
                                        chunk_size_mode=chunk_size_mode,
+                                       number_of_chunks=number_of_chunks,
                                        ram_estimation=float(ram_estimation),
                                        ram_per_chunk=float(ram_per_chunk) * 1024 ** 2)
     independant_raster = []
