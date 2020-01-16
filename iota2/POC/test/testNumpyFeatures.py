789d8399995f2ebc041b0e7d244d3c68ad1a8ed7
Arthur VINCENT <arthur.vincent@c-s.fr>
2020-01-16 13:03:53 +0000

    TEST : ok

--- iota2/POC/test/testNumpyFeatures.py
+++ iota2/POC/test/testNumpyFeatures.py
@@ -119,7 +119,7 @@ class Iota2TestNumpyFeatures(unittest.TestCase):
         function_partial = partial(custom_features)
 
         labels_features_name = ["NDVI_20200101"]
-<<<<<<< HEAD
+
         new_features_path = os.path.join(self.test_working_directory, "DUMMY_test.tif")
         test_array, new_labels, _, _, _ = rasterU.apply_function(otb_pipeline=band_math,
                                                                  labels=labels_features_name,
@@ -129,21 +129,6 @@ class Iota2TestNumpyFeatures(unittest.TestCase):
                                                                  chunck_size_x=5,
                                                                  chunck_size_y=5,
                                                                  ram=128)
-=======
-        new_features_path = os.path.join(
-            self.test_working_directory, "DUMMY_test.tif"
-        )
-        test_array, new_labels, _, _, _ = rasterU.apply_function(
-            otb_pipeline=band_math,
-            labels=labels_features_name,
-            working_dir=self.test_working_directory,
-            function=function_partial,
-            output_path=new_features_path,
-            chunck_size_x=5,
-            chunck_size_y=5,
-            ram=128,
-        )
->>>>>>> 07f19ec6d23a0e8a6c0c6902a4f0b3104d3d9361
         # asserts
 
         # check array' shape consistency
@@ -222,7 +207,6 @@ class Iota2TestNumpyFeatures(unittest.TestCase):
         )
         # prediction
         function_partial = partial(do_predict, model=clf)
-<<<<<<< HEAD
         prediction_path = os.path.join(self.test_working_directory, "Classif_test.tif")
         test_array, new_labels, _, _, _ = rasterU.apply_function(otb_pipeline=band_math,
                                                                  labels=[""],
@@ -232,20 +216,5 @@ class Iota2TestNumpyFeatures(unittest.TestCase):
                                                                  chunck_size_x=5,
                                                                  chunck_size_y=5,
                                                                  ram=128)
-=======
-        prediction_path = os.path.join(
-            self.test_working_directory, "Classif_test.tif"
-        )
-        test_array, new_labels, _, _, _ = rasterU.apply_function(
-            otb_pipeline=band_math,
-            labels=[""],
-            working_dir=self.test_working_directory,
-            function=function_partial,
-            output_path=prediction_path,
-            chunck_size_x=5,
-            chunck_size_y=5,
-            ram=128,
-        )
->>>>>>> 07f19ec6d23a0e8a6c0c6902a4f0b3104d3d9361
         self.assertTrue(os.path.exists(prediction_path))
         self.assertTrue(test_array.shape == (1, 16, 86))
