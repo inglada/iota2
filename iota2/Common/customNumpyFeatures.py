98a260fe15fd11d8da979bc047a6e7cc0d9a06d4
Tardy <tardyb@cesbio.cnes.fr>
2020-01-31 13:57:00 +0000

    WIP: get bands name and indices from config and sensors

--- iota2/Common/customNumpyFeatures.py
+++ iota2/Common/customNumpyFeatures.py
@@ -18,12 +18,38 @@
 class dataContainer():
 
     def __init__(self, cfg_file):
+        from iota2.Common import ServiceConfigFile as SCF
+        from iota2.Sensors.Sensors_container import Sensors_container
         self.data = None  # empty attribute to address data
+        cfg = SCF.serviceConfigFile(cfg_file)
+        # Get the first tile, all the tiles must come from same sensor
+        first_tile = cfg.getParam("chain", "listTile").split(" ")[0]
 
-        # TODO: Get band indices from cfg and sensors
-        bands = ["b1", "b2", "b3"]
-        time_series_indices = [[0, 3], [1, 4], [2, 5]]
-        for band, indices in zip(bands, time_series_indices):
+        def compute_indices_after_gapfilling(sensor, bands):
+            _, dates = sensor.write_interpolation_dates_file(write=False)
+            indices = []
+            for i, band in enumerate(bands):
+                # print(i, band)
+                ind = [i + len(bands) * x for x, dates in enumerate(dates)]
+                print(band, ind)
+                indices.append(ind)
+            return indices
+        # From this tile get enabled sensors
+        sensor_tile_container = Sensors_container(cfg_file,
+                                                  first_tile,
+                                                  working_dir=None)
+        list_of_bands = []
+        time_series_indices = []
+        for sensor in sensor_tile_container.get_enabled_sensors():
+            bands = sensor.stack_band_position
+            tmp_indices = compute_indices_after_gapfilling(sensor, bands)
+            list_of_bands += bands
+            time_series_indices += tmp_indices
+
+        # TODO: handle user feature selection
+        print(list_of_bands)
+        # [[0, 3], [1, 4], [2, 5]]
+        for band, indices in zip(list_of_bands, time_series_indices):
             def get_band(band=band, indices=indices):
                 import numpy as np
                 data = np.take(self.data, indices, axis=2)
