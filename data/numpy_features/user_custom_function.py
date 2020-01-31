4db0e653b5be55d725ed7c96d31e630e7e64155b
Tardy <tardyb@cesbio.cnes.fr>
2020-01-31 15:29:29 +0000

    TEST: add a new function

--- data/numpy_features/user_custom_function.py
+++ data/numpy_features/user_custom_function.py
@@ -4,6 +4,12 @@ def get_identity(self):
     return self.get_band_B2()
 
 
+def get_ndvi(self):
+    coef = ((self.get_band_B8() - self.get_band_B4()) /
+            (self.get_band_B4() + self.get_band_B8()))
+    return coef
+
+
 def custom_function(self):
     print(dir(self))
     print(self.get_band_b1(), self.get_band_b2())
