7891f5e248b8eddb8b3b122d2eef1c189c291adf
Arthur VINCENT <arthur.vincent@c-s.fr>
2020-02-18 08:37:44 +0000

    WIP : yapf + pylint

--- iota2/Sampling/TileArea.py
+++ iota2/Sampling/TileArea.py
@@ -21,14 +21,14 @@ import os
 import math
 from config import Config
 from osgeo import gdal, ogr, osr
-from Common import FileUtils as fu
-from Common import ServiceConfigFile as SCF
-from Common.Utils import run
+from iota2.Common import FileUtils as fu
+from iota2.Common import ServiceConfigFile as SCF
+from iota2.Common.Utils import run
 
 logger = logging.getLogger(__name__)
 
-def AddFieldModel(shpIn, modNum, fieldOut, logger=logger):
 
+def AddFieldModel(shpIn, modNum, fieldOut, logger=logger):
     """
         add a field to a shapeFile and for every feature, add an ID
         IN :
@@ -52,8 +52,9 @@ def AddFieldModel(shpIn, modNum, fieldOut, logger=logger):
         else:
             logger.debug("feature " + str(feat.GetFID()) + " has not geometry")
 
-def CreateModelShapeFromTiles(tilesModel, pathTiles, proj, pathOut, OutSHPname, fieldOut, pathWd):
 
+def CreateModelShapeFromTiles(tilesModel, pathTiles, proj, pathOut, OutSHPname,
+                              fieldOut, pathWd):
     """
         create one shapeFile where all features belong to a model number according to the model description
 
@@ -92,7 +93,9 @@ def CreateModelShapeFromTiles(tilesModel, pathTiles, proj, pathOut, OutSHPname, 
     to_remove = []
     for i in range(len(tilesModel)):
         for j in range(len(tilesModel[i])):
-            to_remove.append(fu.renameShapefile(pathTiles, tilesModel[i][j], "", "", pathToTMP))
+            to_remove.append(
+                fu.renameShapefile(pathTiles, tilesModel[i][j], "", "",
+                                   pathToTMP))
 
     AllTilePath = []
     AllTilePath_ER = []
@@ -100,10 +103,12 @@ def CreateModelShapeFromTiles(tilesModel, pathTiles, proj, pathOut, OutSHPname, 
     for i in range(len(tilesModel)):
         for j in range(len(tilesModel[i])):
             try:
-                ind = AllTilePath.index(pathTiles + "/" + tilesModel[i][j] + ".shp")
+                ind = AllTilePath.index(pathTiles + "/" + tilesModel[i][j] +
+                                        ".shp")
             except ValueError:
                 AllTilePath.append(pathToTMP + "/" + tilesModel[i][j] + ".shp")
-                AllTilePath_ER.append(pathToTMP + "/" + tilesModel[i][j] + "_ERODE.shp")
+                AllTilePath_ER.append(pathToTMP + "/" + tilesModel[i][j] +
+                                      "_ERODE.shp")
 
     for i in range(len(tilesModel)):
         for j in range(len(tilesModel[i])):
@@ -118,10 +123,17 @@ def CreateModelShapeFromTiles(tilesModel, pathTiles, proj, pathOut, OutSHPname, 
         run("rm -r " + pathToTMP)
     else:
         for rm in to_remove:
-            fu.removeShape(rm.replace(".shp",""), [".prj",".shp",".dbf",".shx"])
+            fu.removeShape(rm.replace(".shp", ""),
+                           [".prj", ".shp", ".dbf", ".shx"])
 
-def generateRegionShape(pathTiles, pathToModel, pathOut, fieldOut, cfg,
-                        pathWd, logger=logger):
+
+def generateRegionShape(pathTiles,
+                        pathToModel,
+                        pathOut,
+                        fieldOut,
+                        cfg,
+                        pathWd,
+                        logger=logger):
     """
         create one shapeFile where all features belong to a model number according to the model description
 
@@ -153,22 +165,25 @@ def generateRegionShape(pathTiles, pathToModel, pathOut, fieldOut, cfg,
 
     tmp_proj = cfg.getParam('GlobChain', 'proj')
     proj = int(tmp_proj.split(":")[-1])
-    
+
     region = []
     AllTiles = fu.FileSearch_AND(pathTiles, False, ".shp")
     region.append(AllTiles)
-    
+
     if not pathOut:
-        pathOut = os.path.join(cfg.getParam("chain", "outputPath") , "MyRegion.shp")
+        pathOut = os.path.join(cfg.getParam("chain", "outputPath"),
+                               "MyRegion.shp")
 
     p_f = pathOut.replace(" ", "").split("/")
     outName = p_f[-1].split(".")[0]
-    
+
     pathMod = ""
-    for i in range(1, len(p_f)-1):
-        pathMod = pathMod+"/"+p_f[i]
-    
-    CreateModelShapeFromTiles(region, pathTiles, int(proj), pathMod, outName, fieldOut, pathWd)
+    for i in range(1, len(p_f) - 1):
+        pathMod = pathMod + "/" + p_f[i]
+
+    CreateModelShapeFromTiles(region, pathTiles, int(proj), pathMod, outName,
+                              fieldOut, pathWd)
+
 
 if __name__ == "__main__":
 
