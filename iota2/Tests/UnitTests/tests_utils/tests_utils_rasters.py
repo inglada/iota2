bebbc894b86d6ee57d94582a79b5cc251174f707
Tardy <tardyb@cesbio.cnes.fr>
2020-02-21 14:34:11 +0000

    WIP: create file for tests raster utils

---
+++ iota2/Tests/UnitTests/tests_utils/tests_utils_rasters.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# =========================================================================
+#   Program:   iota2
+#
+#   Copyright (c) CESBIO. All rights reserved.
+#
+#   See LICENSE for details.
+#
+#   This software is distributed WITHOUT ANY WARRANTY; without even
+#   the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+#   PURPOSE.  See the above copyright notices for more information.
+#
+# =========================================================================
+"""
+This module offers some tools for writing tests
+"""
