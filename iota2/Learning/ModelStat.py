7975aa55816698cbbc22d50c93a36b50d6ad43f3
Tardy <tardyb@cesbio.cnes.fr>
2020-02-24 07:48:57 +0000

    WIP: remove config file for ModelStat.py

--- iota2/Learning/ModelStat.py
+++ iota2/Learning/ModelStat.py
@@ -17,11 +17,12 @@
 import argparse
 import os
 from config import Config
-from Learning import GetModel as GM
-from Common import FileUtils as fu
-from Common import ServiceConfigFile as SCF
+from iota2.Learning import GetModel as GM
+from iota2.Common import FileUtils as fu
 
-def generateStatModel(pathShapes, pathToTiles, pathToStats, pathToCmdStats, pathWd, cfg):
+
+def generateStatModel(pathShapes, pathToTiles, pathToStats, pathToCmdStats,
+                      pathWd, output_path, classifier, stack_ind):
 
     AllCmd = []
     #remove splited shape
@@ -29,17 +30,18 @@ def generateStatModel(pathShapes, pathToTiles, pathToStats, pathToCmdStats, path
     classifier = cfg.getParam('argTrain', 'classifier')
     pathConf = cfg.pathConf
 
-    allShape = fu.fileSearchRegEx(outputPath+"/dataAppVal/*.shp")
+    allShape = fu.fileSearchRegEx(outputPath + "/dataAppVal/*.shp")
     for currentShape in allShape:
         #name = currentShape.split("/")[-1]
         path, name = os.path.split(currentShape)
         if len(name.split("_")[2].split("f")) > 1:
             fold = name.split("_")[2].split("f")[-1]
             #path = currentShape.split("/")[0]
-            nameToRm = name.replace("f"+fold, "").replace(".shp", "")
-            print("remove : "+path+"/"+nameToRm+".shp")
-            if os.path.exists(path+"/"+nameToRm+".shp"):
-                fu.removeShape(path+"/"+nameToRm, [".prj", ".shp", ".dbf", ".shx"])
+            nameToRm = name.replace("f" + fold, "").replace(".shp", "")
+            print("remove : " + path + "/" + nameToRm + ".shp")
+            if os.path.exists(path + "/" + nameToRm + ".shp"):
+                fu.removeShape(path + "/" + nameToRm,
+                               [".prj", ".shp", ".dbf", ".shx"])
 
     modTiles = GM.getModel(pathShapes)
     Stack_ind = fu.getFeatStackName(pathConf)
@@ -47,29 +49,60 @@ def generateStatModel(pathShapes, pathToTiles, pathToStats, pathToCmdStats, path
     for mod, Tiles in modTiles:
         allpath = ""
         for tile in Tiles:
-            pathToFeat = pathToTiles+"/"+tile+"/Final/"+Stack_ind
-            allpath = allpath+" "+pathToFeat+" "
+            pathToFeat = pathToTiles + "/" + tile + "/Final/" + Stack_ind
+            allpath = allpath + " " + pathToFeat + " "
         if classifier == "svm":
-            cmd = "otbcli_ComputeImagesStatistics -il "+allpath+" -out "+pathToStats+"/Model_"+str(mod)+".xml"
+            cmd = "otbcli_ComputeImagesStatistics -il " + allpath + " -out " + pathToStats + "/Model_" + str(
+                mod) + ".xml"
         else:
             cmd = "echo 'radom forest does not need stats'"
         AllCmd.append(cmd)
 
-    fu.writeCmds(pathToCmdStats+"/stats.txt", AllCmd)
+    fu.writeCmds(pathToCmdStats + "/stats.txt", AllCmd)
     return AllCmd
+
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description="This function compute the statistics for a model compose by N tiles")
-    parser.add_argument("-shapesIn", help="path to the folder which ONLY contains shapes for the classification (learning and validation) (mandatory)", dest="pathShapes", required=True)
-    parser.add_argument("-tiles.path", dest="pathToTiles", help="path where tiles are stored (mandatory)", required=True)
-    parser.add_argument("-Stats.out", dest="pathToStats", help="path where all statistics will be stored (mandatory)", required=True)
-    parser.add_argument("-Stat.out.cmd", dest="pathToCmdStats", help="path where all statistics cmd will be stored in a text file(mandatory)", required=True)
-    parser.add_argument("--wd", dest="pathWd", help="path to the working directory", default=None, required=False)
-    parser.add_argument("-conf", help="path to the configuration file which describe the learning method (mandatory)", dest="pathConf", required=True)
+    parser = argparse.ArgumentParser(
+        description=
+        "This function compute the statistics for a model compose by N tiles")
+    parser.add_argument(
+        "-shapesIn",
+        help=
+        "path to the folder which ONLY contains shapes for the classification (learning and validation) (mandatory)",
+        dest="pathShapes",
+        required=True)
+    parser.add_argument("-tiles.path",
+                        dest="pathToTiles",
+                        help="path where tiles are stored (mandatory)",
+                        required=True)
+    parser.add_argument(
+        "-Stats.out",
+        dest="pathToStats",
+        help="path where all statistics will be stored (mandatory)",
+        required=True)
+    parser.add_argument(
+        "-Stat.out.cmd",
+        dest="pathToCmdStats",
+        help=
+        "path where all statistics cmd will be stored in a text file(mandatory)",
+        required=True)
+    parser.add_argument("--wd",
+                        dest="pathWd",
+                        help="path to the working directory",
+                        default=None,
+                        required=False)
+    parser.add_argument(
+        "-conf",
+        help=
+        "path to the configuration file which describe the learning method (mandatory)",
+        dest="pathConf",
+        required=True)
     args = parser.parse_args()
 
     # load configuration file
     cfg = SCF.serviceConfigFile(args.pathConf)
 
-    generateStatModel(args.pathShapes, args.pathToTiles, args.pathToStats, args.pathToCmdStats, args.pathWd, cfg)
+    generateStatModel(args.pathShapes, args.pathToTiles, args.pathToStats,
+                      args.pathToCmdStats, args.pathWd, cfg)
