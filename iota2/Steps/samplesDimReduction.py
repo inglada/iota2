27c78a4d4fa50467742350410bca80c3f6207a34
Arthur VINCENT <arthur.vincent@c-s.fr>
2020-02-27 08:02:42 +0000

    WIP : wrong call

--- iota2/Steps/samplesDimReduction.py
+++ iota2/Steps/samplesDimReduction.py
@@ -15,20 +15,25 @@
 # =========================================================================
 import os
 
-from Steps import IOTA2Step
-from Common import ServiceConfigFile as SCF
+from iota2.Steps import IOTA2Step
+from iota2.Common import ServiceConfigFile as SCF
+
 
 class samplesDimReduction(IOTA2Step.Step):
     def __init__(self, cfg, cfg_resources_file, workingDirectory=None):
         # heritage init
         resources_block_name = "dimensionalityReduction"
-        super(samplesDimReduction, self).__init__(cfg, cfg_resources_file, resources_block_name)
+        super(samplesDimReduction, self).__init__(cfg, cfg_resources_file,
+                                                  resources_block_name)
 
         # step variables
         self.workingDirectory = workingDirectory
-        self.output_path = SCF.serviceConfigFile(self.cfg).getParam('chain', 'outputPath')
-        self.targetDimension = SCF.serviceConfigFile(self.cfg).getParam('dimRed', 'targetDimension')
-        self.reductionMode = SCF.serviceConfigFile(self.cfg).getParam('dimRed', 'reductionMode')
+        self.output_path = SCF.serviceConfigFile(self.cfg).getParam(
+            'chain', 'outputPath')
+        self.targetDimension = SCF.serviceConfigFile(self.cfg).getParam(
+            'dimRed', 'targetDimension')
+        self.reductionMode = SCF.serviceConfigFile(self.cfg).getParam(
+            'dimRed', 'reductionMode')
 
     def step_description(self):
         """
@@ -43,8 +48,8 @@ class samplesDimReduction(IOTA2Step.Step):
         ------
             the return could be and iterable or a callable
         """
-        from Sampling import DimensionalityReduction as DR
-        return DR.BuildIOSampleFileLists(self.output_path)
+        from iota2.Sampling import DimensionalityReduction as DR
+        return DR.build_io_sample_file_lists(self.output_path)
 
     def step_execute(self):
         """
@@ -54,11 +59,9 @@ class samplesDimReduction(IOTA2Step.Step):
             the function to execute as a lambda function. The returned object
             must be a lambda function.
         """
-        from Sampling import DimensionalityReduction as DR
-        step_function = lambda x: DR.SampleDimensionalityReduction(x,
-                                                                   self.output_path,
-                                                                   self.targetDimension,
-                                                                   self.reductionMode)
+        from iota2.Sampling import DimensionalityReduction as DR
+        step_function = lambda x: DR.sample_dimensionality_reduction(
+            x, self.output_path, self.targetDimension, self.reductionMode)
         return step_function
 
     def step_outputs(self):
