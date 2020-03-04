48f7292a0fb19240855349426c404302f9d8d18d
Tardy <tardyb@cesbio.cnes.fr>
2020-03-04 15:21:20 +0000

    BUG: samples_shape_na not defined

--- iota2/Sampling/VectorSampler.py
+++ iota2/Sampling/VectorSampler.py
@@ -797,6 +797,7 @@ def generate_samples_classif_mix(folder_sample: str,
         work_dir, "{}_nonAnnual_selection.sqlite".format(current_tile))
     annual_shape = os.path.join(
         work_dir, "{}_annual_selection.sqlite".format(current_tile))
+    # garde toutes les classes pérennes
     nb_feat_nannu = extract_class(sample_selection, non_annual_shape,
                                   all_class, data_field)
 
@@ -866,14 +867,14 @@ def generate_samples_classif_mix(folder_sample: str,
 
     merge_name = train_shape.split("/")[-1].replace(".shp", "_selectionMerge")
     sample_selection = os.path.join(working_directory, f"{merge_name}.sqlite")
-
+    input(f'{annu_repartition}{nb_feat_nannu} {nb_feat_annu} {annual_points}')
     if (nb_feat_nannu > 0) and (nb_feat_annu > 0 and annual_points):
         fu.mergeSQLite(merge_name, working_directory,
                        [non_annual_shape, annual_shape])
 
     elif (nb_feat_nannu > 0) and not (nb_feat_annu > 0 and annual_points):
         # TODO: define SampleSel_NA (currently undefined)
-        shutil.copy(sample_sel_na, sample_selection)
+        shutil.copy(non_annual_shape, sample_selection)
     elif not (nb_feat_nannu > 0) and (nb_feat_annu > 0 and annual_points):
         # TODO: define annualShape (currently undefined)
         shutil.copy(annual_shape, sample_selection)
