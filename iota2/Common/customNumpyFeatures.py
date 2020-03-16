d81acc8eaaeaf146b9c4456b88549df54877b1d1
Tardy <tardyb@cesbio.cnes.fr>
2020-03-16 07:21:31 +0000

    WIP: remove config file

--- iota2/Common/customNumpyFeatures.py
+++ iota2/Common/customNumpyFeatures.py
@@ -13,84 +13,100 @@
 #   PURPOSE.  See the above copyright notices for more information.
 #
 # =========================================================================
+from typing import Dict, Union, List, Optional
+
+sensors_params_type = Dict[str, Union[str, List[str], int]]
 
 
-class dataContainer:
-    def __init__(self, cfg_file):
-        from iota2.Common import ServiceConfigFile as SCF
-        from iota2.Sensors.Sensors_container import Sensors_container
+class data_container:
+    """ This class contains all methods to access data in image"""
+    def __init__(self,
+                 tile_name: str,
+                 output_path: str,
+                 sensors_parameters: sensors_params_type,
+                 working_dir: Optional[str] = None):
+        from iota2.Sensors.Sensors_container import sensors_container
 
         self.data = None  # empty attribute to address data
-        cfg = SCF.serviceConfigFile(cfg_file)
+
         # Get the first tile, all the tiles must come from same sensor
-        first_tile = cfg.getParam("chain", "listTile").split(" ")[0]
 
         def compute_indices_after_gapfilling(sensor, bands):
             _, dates = sensor.write_interpolation_dates_file(write=False)
             indices = []
             for i, band in enumerate(bands):
                 # print(i, band)
-                ind = [i + len(bands) * x for x, dates in enumerate(dates)]
+                ind = [i + len(bands) * x for x, _ in enumerate(dates)]
                 print(band, ind)
                 indices.append(ind)
             return indices
 
         # From this tile get enabled sensors
-        sensor_tile_container = Sensors_container(
-            cfg_file, first_tile, working_dir=None
-        )
+        sensor_tile_container = sensors_container(tile_name, working_dir,
+                                                  output_path,
+                                                  **sensors_parameters)
 
         list_of_bands = []
         time_series_indices = []
+        list_sensors = []
         for sensor in sensor_tile_container.get_enabled_sensors():
+
             bands = sensor.stack_band_position
             tmp_indices = compute_indices_after_gapfilling(sensor, bands)
             list_of_bands += bands
             time_series_indices += tmp_indices
-
+            list_sensors += [sensor.name] * len(bands)
         # TODO: handle user feature selection
         print(list_of_bands)
         # [[0, 3], [1, 4], [2, 5]]
-        for band, indices in zip(list_of_bands, time_series_indices):
 
-            def get_band(band=band, indices=indices):
+        for band, indices, sensor in zip(list_of_bands, time_series_indices,
+                                         list_sensors):
+
+            def get_band(indices=indices):
                 import numpy as np
-
                 data = np.take(self.data, indices, axis=2)
                 return data
 
-            setattr(dataContainer, "get_band_{}".format(band), get_band)
+            setattr(data_container, f"get_{sensor}_band_{band}", get_band)
 
 
-class customNumpyFeatures(dataContainer):
-    def __init__(self, cfg_file):
+class custom_numpy_features(data_container):
+    """ This class add functions provided by an user.
+        and concatenate the results to the original feature stack"""
+    def __init__(self,
+                 tile_name: str,
+                 output_path: str,
+                 sensors_params: sensors_params_type,
+                 code_path: str,
+                 module_name: str,
+                 list_functions: List[str],
+                 working_dir: Optional[str] = None):
         import sys
         import os
         import importlib
         import types  # solves issues about type and inheritance
-        from iota2.Common import ServiceConfigFile as SCF
-
-        super(customNumpyFeatures, self).__init__(cfg_file)
-        cfg = SCF.serviceConfigFile(cfg_file)
-        path = cfg.getParam("Features", "codePath")
-        if path is None:
-            raise ("For use custom features, the codePath must no be None")
-        if not os.path.isdir(path):
-            raise ("Error: {} is not a correct path".format(path))
-        module = cfg.getParam("Features", "namefile")
-        sys.path.insert(0, path)
+        super(custom_numpy_features,
+              self).__init__(tile_name, output_path, sensors_params,
+                             working_dir)
+        if code_path is None:
+            raise "For use custom features, the codePath must no be None"
+        if not os.path.isdir(code_path):
+            raise f"Error: {code_path} is not a correct path"
+        sys.path.insert(0, code_path)
         # TODO: check if module is a module and func a function
-        mod = importlib.import_module(module)
+        mod = importlib.import_module(module_name)
         # Function can be multiple
         self.fun_name_list = []
-        param = cfg.getParam("Features", "functions")
-        if param is not None and len(param) != 0:
-            self.fun_name_list = cfg.getParam("Features", "functions").split(" ")
+
+        if list_functions is not None and len(list_functions) != 0:
+            self.fun_name_list = list_functions
             for fun_name in self.fun_name_list:
                 func = getattr(mod, fun_name)
-                setattr(self, fun_name, types.MethodType(func, dataContainer))
+                setattr(self, fun_name, types.MethodType(func, data_container))
 
     def process(self, data):
+        """ This function apply a set of functions to data"""
         import numpy as np
 
         self.data = data
@@ -102,7 +118,9 @@ class customNumpyFeatures(dataContainer):
                 # feat is a numpy array with shape [row, cols, bands]
                 # check if user defined well labels
                 if len(labels) != feat.shape[2]:
-                    labels = [f"custFeat_{i+1}_b{j+1}" for j in range(feat.shape[2])]
+                    labels = [
+                        f"custFeat_{i+1}_b{j+1}" for j in range(feat.shape[2])
+                    ]
                 new_labels += labels
                 self.data = np.concatenate((self.data, feat), axis=2)
             return self.data, new_labels
