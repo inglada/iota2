91e89bf4656a2ef2dfd3b19a5cd135f41fd73f19
Tardy <tardyb@cesbio.cnes.fr>
2020-03-26 11:54:22 +0000

    WIP: add new indices

--- data/numpy_features/user_custom_function.py
+++ data/numpy_features/user_custom_function.py
@@ -21,3 +21,27 @@ def custom_function_inv(self):
     print(dir(self))
     print(self.get_Sentinel2_band_b1(), self.get_Sentinel2_band_b2())
     return self.get_Sentinel2_band_b1() - self.get_Sentinel2_band_b2()
+
+
+def get_ndsi(self):
+    coef = (self.get_Sentinel2_band_B3() - self.get_Sentinel2_band_B11()) / (
+        self.get_Sentinel2_band_B3() + self.get_Sentinel2_band_B11())
+    labels = [f"ndvi_{i+1}" for i in range(coef.shape[2])]
+    print("out custom features")
+    return coef, labels
+
+
+def get_cari(self):
+    import numpy as np
+    a = (
+        (self.get_Sentinel2_band_B5() - self.get_Sentinel2_band_B3()) / 150
+    ) * 670 + self.get_Sentinel2_band_B4() + (self.get_Sentinel2_band_B3() - (
+        (self.get_Sentinel2_band_B5() - self.get_Sentinel2_band_B3()) / 150) *
+                                              550)
+    b = ((self.get_Sentinel2_band_B5() - self.get_Sentinel2_band_B3()) /
+         (150 * 150)) + 1
+    coef = (self.get_Sentinel2_band_B5() / self.get_Sentinel2_band_B4()) * (
+        (np.sqrt(a * a)) / (np.sqrt(b)))
+    labels = [f"cari_{i+1}" for i in range(coef.shape[2])]
+
+    return coef, labels
