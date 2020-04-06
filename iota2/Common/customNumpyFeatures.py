ac8296da5d3f7364079f6796a72346f3461e0a6c
Tardy <tardyb@cesbio.cnes.fr>
2020-04-06 16:37:50 +0000

    WIP: split custom features and generate features

--- iota2/Common/customNumpyFeatures.py
+++ iota2/Common/customNumpyFeatures.py
@@ -13,7 +13,12 @@
 #   PURPOSE.  See the above copyright notices for more information.
 #
 # =========================================================================
+""" This module allow to use python function in an OTB pipeline"""
+import logging
 from typing import Dict, Union, List, Optional
+from rasterio.plot import reshape_as_image
+from iota2.Common import FileUtils as fu
+LOGGER = logging.getLogger(__name__)
 
 sensors_params_type = Dict[str, Union[str, List[str], int]]
 
@@ -58,7 +63,6 @@ class data_container:
             list_sensors += [sensor.name] * len(bands)
         # TODO: handle user feature selection
         print(list_of_bands)
-        # [[0, 3], [1, 4], [2, 5]]
 
         for band, indices, sensor in zip(list_of_bands, time_series_indices,
                                          list_sensors):
@@ -129,3 +133,117 @@ class custom_numpy_features(data_container):
         except Exception as e:
             print(f"Error during custom_features computation")
             print(e)
+
+
+def compute_custom_features(tile,
+                            output_path,
+                            sensors_parameters,
+                            module_path,
+                            list_functions,
+                            otb_pipeline,
+                            feat_labels,
+                            path_wd,
+                            chunk_size_mode,
+                            targeted_chunk,
+                            number_of_chunks,
+                            chunk_size_x,
+                            chunk_size_y,
+                            logger=LOGGER):
+    """
+    This function apply a list of function to an otb pipeline data and
+    return an otbimage object.
+    Parameters
+    ----------
+
+    Return
+    ------
+
+    """
+    from iota2.Common.rasterUtils import insert_external_function_to_pipeline
+    from functools import partial
+
+    cust = custom_numpy_features(tile, output_path, sensors_parameters,
+                                 module_path, list_functions)
+    function_partial = partial(cust.process)
+    labels_features_name = ""
+    # TODO : how to feel labels_features_name ?
+    # The output path is empty to ensure the image was not writed
+    (feat_array, new_labels, out_transform, _, _,
+     otbimage) = insert_external_function_to_pipeline(
+         otb_pipeline=otb_pipeline,
+         labels=labels_features_name,
+         working_dir=path_wd,
+         chunk_size_mode=chunk_size_mode,
+         function=function_partial,
+         targeted_chunk=targeted_chunk,
+         number_of_chunks=number_of_chunks,
+         chunk_size_x=chunk_size_x,
+         chunk_size_y=chunk_size_y,
+         ram=128,
+     )
+    # rasterio shape [bands, row, cols] OTB shape [row, cols, bands]
+    # use rasterio reshape_as_image function to move axis
+    # otbimage["array"] = reshape_as_image(feat_array)
+    # logger.debug("Numpy image inserted in otb pipeline: "
+    #              f"{fu.memory_usage_psutil()} MB")
+    # # get the chunk size
+    # size = otbimage["array"].shape
+    # # get the chunk origin
+    # origin = out_transform * (0, 0)
+    # # add a demi pixel size to origin
+    # # offset between rasterio (gdal) and OTB
+    # otbimage["origin"].SetElement(0, origin[0] + (otbimage["spacing"][0] / 2))
+    # otbimage["origin"].SetElement(1, origin[1] + (otbimage["spacing"][1] / 2))
+    # # Set buffer image region
+    # # Mandatory to keep the projection in OTB pipeline
+    # otbimage["region"].SetIndex(0, 0)
+    # otbimage["region"].SetIndex(1, 0)
+    # otbimage["region"].SetSize(0, size[1])
+    # otbimage["region"].SetSize(1, size[0])
+    # # TODO: remove this bandmath and return an OTB image
+    # bandmath = otb.Registry.CreateApplication("BandMathX")
+    # bandmath.ImportVectorImage("il", otbimage)
+    # bandmath.SetParameterString("out", features_raster)
+    # bandmath.SetParameterString("exp", "im1")
+    # # bandmath.SetParameterString("ram", "200000")
+    # all_features = bandmath
+    # dep.append(bandmath)
+    # dep.append(otbimage)
+    # new_labels can never be empty
+    crop_otbimage = convert_numpy_array_to_otb_image(otbimage, feat_array,
+                                                     out_transform)
+
+    feat_labels += new_labels
+    return crop_otbimage, feat_labels
+
+
+def convert_numpy_array_to_otb_image(otbimage,
+                                     array,
+                                     out_transform,
+                                     logger=LOGGER):
+    """
+        This function allow to convert a numpy array to an otb image
+    Parameters
+    ----------
+    otbimage : an otb image dictionary
+    array : a rasterio array (shape is [bands, row, cols])
+    out_transform : the geotransform corresponding to array
+    """
+    otbimage["array"] = reshape_as_image(array)
+    logger.debug("Numpy image inserted in otb pipeline: "
+                 f"{fu.memory_usage_psutil()} MB")
+    # get the chunk size
+    size = otbimage["array"].shape
+    # get the chunk origin
+    origin = out_transform * (0, 0)
+    # add a demi pixel size to origin
+    # offset between rasterio (gdal) and OTB
+    otbimage["origin"].SetElement(0, origin[0] + (otbimage["spacing"][0] / 2))
+    otbimage["origin"].SetElement(1, origin[1] + (otbimage["spacing"][1] / 2))
+    # Set buffer image region
+    # Mandatory to keep the projection in OTB pipeline
+    otbimage["region"].SetIndex(0, 0)
+    otbimage["region"].SetIndex(1, 0)
+    otbimage["region"].SetSize(0, size[1])
+    otbimage["region"].SetSize(1, size[0])
+    return otbimage
