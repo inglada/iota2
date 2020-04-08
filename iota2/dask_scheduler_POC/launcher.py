5f64b9847e177e224c8fa7bf01c8e6f2a8d83fc9
Arthur VINCENT <arthur.vincent@c-s.fr>
2020-04-08 06:59:34 +0000

    REFAC : LocalCluster

--- iota2/dask_scheduler_POC/launcher.py
+++ iota2/dask_scheduler_POC/launcher.py
@@ -13,6 +13,7 @@
 #   PURPOSE.  See the above copyright notices for more information.
 #
 # =========================================================================
+import os
 import argparse
 
 from iota2_step import step
@@ -21,6 +22,12 @@ from second_step import second_step
 from third_step import third_step
 from forth_step import forth_step
 from fifth_step import fifth_step
+from fusion_step import fusion_step
+from mosaic_step import mosaic_step
+
+from dask.distributed import Client
+from dask.distributed import LocalCluster
+from dask_jobqueue import PBSCluster
 
 if __name__ == "__main__":
     PARSER = argparse.ArgumentParser(description="iota2 dask scheduling POC")
@@ -42,7 +49,13 @@ if __name__ == "__main__":
         third_step(running_directory),
         forth_step(running_directory),
         fifth_step(running_directory),
+        fusion_step(running_directory),
+        mosaic_step(running_directory)
     ]
+    # list_of_sorted_i2_steps = [
+    #     first_step(running_directory),
+    #     second_step(running_directory)
+    # ]
 
     # get tasks to be launched
     final_graph = step.get_final_i2_exec_graph()
@@ -52,9 +65,16 @@ if __name__ == "__main__":
     # figure path must be a parameter of Iota2.py (output directories not created)
 
     # TODO : be able of using dask dashboard, very useful for production operators to monitor the chain
+    # color="order"
     final_graph.visualize(filename=graph_file)
 
-    # then, launch tasks (it's a blocking line)
-    final_graph.compute()
+    cluster = cluster = LocalCluster(n_workers=1)
+    client = Client(cluster)
+    #final_graph.compute()
 
+    print(f"dashboard available at : {client.dashboard_link}")
+
+    # then, launch tasks (it's a blocking line)
+    res = client.submit(final_graph.compute)
+    res.result()
     # python launcher.py -running_dir /home/uz/vincenta/tmp/dask_scheduler/dask_scheduler -graph_file /home/uz/vincenta/tmp/dask_scheduler/dask_scheduler/graphe_POC.png
