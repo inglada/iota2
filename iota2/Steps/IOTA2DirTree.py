88c47925d658866443bba31aeb2cd03fc486f351
Arthur VINCENT <arthur.vincent@c-s.fr>
2020-04-21 09:12:33 +0000

    WIP : start changing step_description method to a class method

--- iota2/Steps/IOTA2DirTree.py
+++ iota2/Steps/IOTA2DirTree.py
@@ -88,7 +88,8 @@ class IOTA2DirTree(IOTA2Step.Step):
             dir_task, "first_task")
         self.step_tasks.append(task_in_graph)
 
-    def step_description(self):
+    @classmethod
+    def step_description(cls):
         """
         function use to print a short description of the step's purpose
         """
