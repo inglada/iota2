1765da46d6bff6c30beb8c9dbb49a04961b146a4
Arthur VINCENT <arthur.vincent@c-s.fr>
2020-05-20 09:09:28 +0000

    REFAC : fusionsIndecisions.py refactoring according to dask scheduling workflow

--- iota2/Steps/fusionsIndecisions.py
+++ iota2/Steps/fusionsIndecisions.py
@@ -16,8 +16,9 @@
 import os
 
 from iota2.Steps import IOTA2Step
-from iota2.Common import ServiceConfigFile as SCF
 from iota2.Common import FileUtils as fut
+from iota2.Classification import undecision_management as UM
+from iota2.Common import ServiceConfigFile as SCF
 
 
 class fusionsIndecisions(IOTA2Step.Step):
@@ -53,6 +54,44 @@ class fusionsIndecisions(IOTA2Step.Step):
         self.sar_opt_fusion = SCF.serviceConfigFile(self.cfg).getParam(
             'argTrain', 'dempster_shafer_SAR_Opt_fusion')
         self.path_to_img = os.path.join(self.output_path, "features")
+        self.execution_mode = "cluster"
+
+        for model_name, model_meta in self.spatial_models_distribution_no_sub_splits.items(
+        ):
+            for seed in range(self.runs):
+                for tile in model_meta["tiles"]:
+                    fusion_img = os.path.join(
+                        self.output_path, "classif",
+                        f"{tile}_FUSION_model_{model_name}_seed_{seed}.tif")
+                    task = self.i2_task(
+                        task_name=
+                        f"manage_undecision_{tile}_model_{model_name}_seed_{seed}",
+                        log_dir=self.log_step_dir,
+                        execution_mode=self.execution_mode,
+                        task_parameters={
+                            "f": UM.undecision_management,
+                            "path_test": self.output_path,
+                            "path_fusion": fusion_img,
+                            "field_region": self.field_region,
+                            "path_to_img": self.path_to_img,
+                            "path_to_region": self.shape_region,
+                            "no_label_management": self.no_label,
+                            "path_wd": self.working_directory,
+                            "list_indices": list(self.features),
+                            "user_feat_path": self.user_feat_path,
+                            "pix_type": self.pixtype,
+                            "region_vec": self.region_vec,
+                            "user_feat_pattern": self.patterns,
+                            "ds_sar_opt": self.sar_opt_fusion
+                        },
+                        task_resources=self.resources)
+                    if model_meta["nb_sub_model"] > 1:
+                        self.add_task_to_i2_processing_graph(
+                            task,
+                            task_group="tile_tasks_model",
+                            task_sub_group=f"{tile}_{model_name}_{seed}",
+                            task_dep_group="tile_tasks_model",
+                            task_dep_sub_group=[f"{tile}_{model_name}_{seed}"])
 
     @classmethod
     def step_description(cls):
@@ -61,33 +100,3 @@ class fusionsIndecisions(IOTA2Step.Step):
         """
         description = ("Manage indecisions in classification's fusion")
         return description
-
-    def step_inputs(self):
-        """
-        Return
-        ------
-            the return could be and iterable or a callable
-        """
-        from iota2.Common import FileUtils as fut
-        return fut.FileSearch_AND(os.path.join(self.output_path, "classif"),
-                                  True, "_FUSION_")
-
-    def step_execute(self):
-        """
-        Return
-        ------
-        lambda
-            the function to execute as a lambda function. The returned object
-            must be a lambda function.
-        """
-        from iota2.Classification import undecision_management as UM
-        return lambda x: UM.undecision_management(
-            self.output_path, x, self.field_region, self.path_to_img, self.
-            shape_region, self.no_label, self.working_directory,
-            list(self.features), self.user_feat_path, self.pixtype, self.
-            region_vec, self.patterns, self.sar_opt_fusion)
-
-    def step_outputs(self):
-        """
-        """
-        pass
