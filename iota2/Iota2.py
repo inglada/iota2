110d4714470e9ea8d977b49e66481f174d13a82d
Arthur VINCENT <arthur.vincent@c-s.fr>
2020-05-28 07:27:32 +0000

    REFAC : move launchin to a function

--- iota2/Iota2.py
+++ iota2/Iota2.py
@@ -19,6 +19,39 @@ from dask.distributed import LocalCluster
 
 from iota2 import Iota2Builder as chain
 
+
+def run(args):
+    """
+    """
+    cfg = SCF.serviceConfigFile(args.configPath)
+    cfg.checkConfigParameters()
+    chain_to_process = chain.iota2(cfg.pathConf, args.config_ressources)
+    if args.start == args.end == 0:
+        all_steps = chain_to_process.get_steps_number()
+        args.start = all_steps[0]
+        args.end = all_steps[-1]
+
+    first_step_index = args.start - 1
+    last_step_index = args.end - 1
+
+    final_graph = chain_to_process.get_final_i2_exec_graph(
+        first_step_index, last_step_index, args.graph_figure)
+
+    print(
+        chain_to_process.print_step_summarize(
+            args.start, args.end, args.config_ressources is not None))
+
+    cluster = LocalCluster(n_workers=1)
+    client = Client(cluster)
+
+    print(f"dashboard available at : {client.dashboard_link}")
+
+    # final_graph.compute()
+    if not args.only_summary:
+        res = client.submit(final_graph.compute)
+        res.result()
+
+
 if __name__ == "__main__":
 
     from iota2.Common import ServiceConfigFile as SCF
@@ -68,29 +101,8 @@ if __name__ == "__main__":
                         type=int,
                         default=None)
     args = parser.parse_args()
-    cfg = SCF.serviceConfigFile(args.configPath)
-    cfg.checkConfigParameters()
-    chain_to_process = chain.iota2(cfg.pathConf, args.config_ressources)
-    if args.start == args.end == 0:
-        all_steps = chain_to_process.get_steps_number()
-        args.start = all_steps[0]
-        args.end = all_steps[-1]
-
-    first_step_index = args.start - 1
-    last_step_index = args.end - 1
-    print(
-        chain_to_process.print_step_summarize(
-            args.start, args.end, args.config_ressources is not None))
-    final_graph = chain_to_process.get_final_i2_exec_graph(
-        first_step_index, last_step_index, args.graph_figure)
-
-    cluster = LocalCluster(n_workers=1)
-    client = Client(cluster)
-
-    print(f"dashboard available at : {client.dashboard_link}")
-
-    # final_graph.compute()
+    run(args)
     # launch chain
-    if not args.only_summary:
-        res = client.submit(final_graph.compute)
-        res.result()
+    # if not args.only_summary:
+    #     res = client.submit(final_graph.compute)
+    #     res.result()
